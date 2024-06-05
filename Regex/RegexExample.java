69624abcbca7dbfa8a44c99fc7fca2cae7959f06
shanae-H <sheslop@griddynamics.com>
2024-06-05 10:27:56 -0500

    modify function to get order id

--- Regex/RegexExample.java
+++ Regex/RegexExample.java
@@ -4,7 +4,7 @@ public class RegexExample {
     Pattern pattern;
     Matcher matcher;
     String string1 = "app=edi_adapter_converter wingtipsTrace=8faeae6709355291 INFO  OrderCreateClient - action=EDIOrderSent originalFilename=Integration_test_Contract customerName=0005084863 orderUUID=d34149d8-88ab-4791-bb0a-46c96e034200 poNum=Test_TS5155079515 lineCount=3";
-
+    String string2="test 2667843 (test_email@griddynamics.com) test 67483 some string";
     //Task 1
     public boolean hasOrderUuid(){
         pattern= Pattern.compile("orderUUID");
@@ -15,17 +15,18 @@ public class RegexExample {
         return false;
     }
 
-    public String getOrderUuid(){
-        boolean found=false;
-        pattern= Pattern.compile("orderUUID=([^\\s]*)");
+    //Task 2
+    public void getOrderUuid(){
+        pattern= Pattern.compile("orderUUID=(\\S*)");
         matcher= pattern.matcher(string1);
         if (matcher.find()){
-            System.out.println("I found the text "+matcher.group()+" starting at index "+
-                    matcher.start()+" and ending at index "+matcher.end());
-            return "true";
+            System.out.println("Found "+ matcher.group());
+        }else{
+            System.out.println("Cannot find order");
         }
-        return "false";
     }
+
+
 
 
     public static void main(String[] args){
